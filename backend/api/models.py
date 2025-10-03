06f03db59dcb041415e399efedfecf960419f8c0
Jay7017a <jayasreebanda510@gmail.com>
2025-10-03 16:11:20 +0530

    Update models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here.from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    owner = models.ForeignKey(User, related_name="tasks", on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def _str_(self):
+        return self.title
